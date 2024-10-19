c1a1c63a615fdbc5293f7f4e9550a4ba6a3bebf7
Harshitha <hthimmap@calpoly.edu>
2024-10-19 15:33:21 -0700

    Part 1 + 2 + 3 without test cases

--- hw1.py
+++ hw1.py
@@ -13,7 +13,6 @@ def vowel_count(word:str)-> int:
     print(count)
 
 
-vowel_count("barn")
 
 # Part 2
 # Define a function named short_lists that takes one parameter of type list[list[int]]. This function must return a new list
@@ -26,10 +25,31 @@ def short_lists(new:list[list[int]])->list:
             new_list.append(new[i])
     print(new_list)
 
-short_lists([[3,2],[4,5,6],[5,6,]])
+
 
 
 # Part 3
+# Define a function named ascending_pairs that takes one parameter of type list[list[int]]. This function must return a new list
+# with elements (the nested lists) matching those of the input list (in the same order) but such that any nested list of length 2
+# in the result has its elements in ascending order (really, non-descending; i.e., the first element will be less than or equal to
+# the second). The elements of nested lists of length not equal to 2 must remain in the same order as in the input.
+
+def ascending_pairs(new: list[list[int]]) -> list:
+    new_list = []
+    for i in range(len(new)):
+        if len(new[i]) == 2:
+            e_list = []
+            if new[i][0] >= new[i][1]:
+                e_list.append(new[i][1])
+                e_list.append(new[i][0])
+                new_list.append(e_list)
+            elif new[i][0] <= new[i][1]:
+                e_list.append(new[i][0])
+                e_list.append(new[i][1])
+                new_list.append(e_list)
+        else:
+            new_list.append(new[i])
+    print(new_list)
 
 
 # Part 4
