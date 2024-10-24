2c949b0879d64850dcdfe7c2c657180211290a69
Harshitha <hthimmap@calpoly.edu>
2024-10-23 23:02:40 -0700

    update

--- hw1.py
+++ hw1.py
@@ -1,3 +1,4 @@
+import math
 import data
 
 # Write your functions for each part in the space below.
@@ -10,7 +11,7 @@ def vowel_count(word:str)-> int:
     for x in char:
         if x in vowels:
             count = count+1
-    print(count)
+    return count
 
 
 
@@ -23,8 +24,7 @@ def short_lists(new:list[list[int]])->list:
     for i in range(len(new)):
         if(len(new[i]) == 2):
             new_list.append(new[i])
-    print(new_list)
-
+    return new_list
 
 
 
@@ -49,21 +49,72 @@ def ascending_pairs(new: list[list[int]]) -> list:
                 new_list.append(e_list)
         else:
             new_list.append(new[i])
-    print(new_list)
+    return new_list
 
 
 # Part 4
+# Define a function named add_prices with two parameters each of type Price (defined in the provided files).
+# This function must compute and return the sum of the input prices as a new Price object but initalized such that the number
+# of cents is not above 99. There are multiple valid approaches to the implementation of this function including with or without
+# the use of a conditional statement.
+# Your implementation may assume that the prices passed to the function are properly formed such that the cents are within
+# the range 0-99.
+
+def add_prices(price1,price2):
+    total_cents = price1.cents + price2.cents
+    total_dollars = price1.dollars+price2.dollars + (total_cents//100)
+
+    total_cents = total_cents%100
+    return total_dollars+total_cents
 
 
 # Part 5
+#Define a function named rectangle_area with one parameter of type Rectangle (defined in the provided files). This function must
+# compute and return the area of the provided rectangle with the assumption that the rectangle is properly axis-aligned (
+# i.e., the top-left corner is above and to the left of the bottom-right corner, the vertical sides of the rectangle are parallel
+# to the y-axis, and the horizontal sides of the rectangle are parallel to the x-axis).
 
+def rectangle_area(points: data.Rectangle)-> float:
+    y_distance = points.top_left.y-points.bottom_right.y
+    x_distance = points.bottom_right.x-points.top_left.x
+    area = y_distance*x_distance
+    return area
 
 # Part 6
+def books_by_author(author:str,b_list: list[data.Book]):
+    return_list = []
+    for i in range(len(b_list)):
+        for x in b_list[i].authors:
+            if x == author:
+                return_list.append(b_list[i].title)
+    return return_list
 
 
 # Part 7
+def circle_bound(fun:data.Rectangle):
+    y_distance = (fun.top_left.y - fun.bottom_right.y)/2
+    x_distance = (fun.bottom_right.x - fun.top_left.x)/2
+    middle_x = fun.top_left.x + x_distance
+    middle_y = fun.top_left.y - y_distance
+    pythagX = pow((x_distance*2),2)
+    print(pythagX)
+    pythagY = pow((y_distance*2),2)
+    radius = math.sqrt(pythagX+pythagY)
+
+    return data.Circle((middle_x,middle_y),radius)
+
 
 
 # Part 8
 
+def below_pay_average(list: list[data.Employee]):
+    # find average pay of all employees
+    salary = 0
+    for i in list:
+        money = list[i].pay_rate
+        salary = salary+money
+    average = salary/len(list)
+    for i in list:
+        if list[i].pay_rate < average:
+            return list[i].name
 
--- hw1_test.py
+++ hw1_test.py
@@ -4,28 +4,74 @@ import unittest
 
 
 # Write your test cases for each part below.
+# class TestCases(unittest.TestCase):
+# Part 1
+# def test_first_element_1(self):
+#   input = [[1, 2], [3, 4]]
+#  result = lab4.first_element(input)
+# expected = [1, 3]
+# self.assertEqual(expected, result)
 
 class TestCases(unittest.TestCase):
-    # Part 1
 
+    # TODO: look up test coverage :)
+    # Part 1
+    def test_vowel_count(self):
+        input = "hello"
+        result = hw1.vowel_count(input)
+        expected = 2
+        self.assertEqual(expected, result)
 
     # Part 2
-
+    def test_short_lists(self):
+        input = [[3,5,6],[4,3],[3,2]]
+        result = hw1.short_lists(input)
+        expected = [[4,3],[3,2]]
+        self.assertEqual(expected,result)
 
     # Part 3
-
+    def test_ascending_pairs(self):
+        input = [[4,6,7],[4,3],[2,7]]
+        result = hw1.ascending_pairs(input)
+        expected = [[4,6,7],[3,4],[2,7]]
+        self.assertEqual(expected,result)
 
     # Part 4
-
+    def test_add_prices(self):
+        money1 = data.Price(4,50)
+        money2 = data.Price(5,50)
+        result = hw1.add_prices(money1,money2)
+        expected = 10.00
+        self.assertEqual(expected,result)
 
     # Part 5
+    def test_rectangle_area(self):
+        point1 = data.Point(3,4)
+        point2 = data.Point(2,8)
+        myRect = data.Rectangle(point1, point2)
+        result = hw1.rectangle_area(myRect)
+        expected = 4
+        self.assertEqual(expected,result)
+
 
 
     # Part 6
-
+    def test_books_by_author(self):
+        book1 = data.Book(["Dr.Seuss","another author"],"Oh the places you'll go")
+        book2 = data.Book(["E.B white","different author"],"Stuart Little")
+        #input = "Dr.Seuss",[("Dr.Seuss","Oh the places you'll go"),["E.B white","Stuart Little"]]
+        result = hw1.books_by_author("Dr.Seuss",[book1,book2])
+        expected = ["Oh the places you'll go"]
+        self.assertEqual(expected, result)
 
     # Part 7
-
+    def test_circle_bound(self):
+        point1 = data.Point(4,9)
+        point2 = data.Point(8,5)
+        startingRect = data.Rectangle(point1,point2)
+        result = hw1.circle_bound(startingRect)
+        expected = data.Circle((6.0,7.0),2.0)
+        self.assertEqual(expected,result)
 
     # Part 8
 
